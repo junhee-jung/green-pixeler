7b32b63b9aa86753165504568f4a09f924e366de
Jun Hee Jung <49542143+junhee-jung@users.noreply.github.com>
2024-09-11 14:01:59 +0200

    Update HSV_green_pixeler.py

--- HSV_green_pixeler.py
+++ HSV_green_pixeler.py
@@ -9,20 +9,21 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy
 
-#change below
+#change below file location / image type JPG / PNG / JPEG?
 os.chdir(r"FILE_LOCATION")
 cwd=glob.glob("./*.jpg")
+#change file name / location - this will contain all the pixel value
 datt=open(r"FILE_LOCATION\no_stem.csv","a")
 datt.write("new_run \n")
 datt.close()
 
 def green_pixelar(i):
-    #change below
+    #change file name / location - this will contain all the pixel value - but same as last time!!!
     datt=open(r"FILE_LOCATION\no_stem.csv","a")
     frame = cv2.imread(i, cv2.IMREAD_COLOR)
     height, width, channels = frame.shape
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #change below
+    #change below find your favourite threashhold and use it
     lower = np.array([0,0 ,0]) #for green roughly 5, 70,70      for yellow roughly 0, 70, 71 
     upper = np.array([255, 255, 255]) #for green roughly 75, 250, 215 for yellow roughly 30, 234, 234)
     mask = cv2.inRange(hsv, lower, upper)
@@ -34,7 +35,8 @@ def green_pixelar(i):
     plt.subplot(212)
     result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
     plt.imshow(result,  cmap = plt.cm.Spectral)
-    #change below
+    #change below adjust DPI if you want the output file to be smaller / make the scipt run much much faster
+    #save image location different to original file - it will probably overwrite existing images ...
     plt.savefig(r"FILE_LOCATION2"+"\\"+i.split('\\')[1],dpi=2500)
     datt.write(i.split('\\')[1]+"\t"+str(countp)+"\n")
     datt.close()
@@ -50,8 +52,12 @@ def green_pixelar(i):
     del hsv  
     matplotlib.use('Agg')
 
+
+#this bit contains the code that somehow allows you to run the script as a multi-threaded script change Pool() value to increase / decrease the number of "thread" used  
+#(I think technically not using a thread per se but running mulitple of the same script) so even if you have 4 cores you can make pool be 48 it will just run veeeeery slow
 if __name__ == '__main__':
     start = time.time()
+    #change below to increase / decrease no of cores
     pool = Pool(6)  
     pool.map(green_pixelar, cwd) 
     end = time.time()
